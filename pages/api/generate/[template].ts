f553276ede3146fb183f47edad7ec63643320084
jozefhruska <jo.hruska27@gmail.com>
2022-02-01 18:11:06 +0100

    feat: add support for multiple templates

---
+++ pages/api/generate/[template].ts
@@ -0,0 +1,49 @@
+import type { NextApiHandler } from 'next';
+import { getScreenshot } from '../../../helpers/images';
+import { ZodError } from 'zod';
+import { TemplateKey } from '../../../templates';
+import { getArticleTemplateHtml } from '../../../templates/article';
+
+const GenerateApiHandler: NextApiHandler = async (req, res) => {
+  try {
+    if (typeof req.query.template !== 'string') {
+      // noinspection ExceptionCaughtLocallyJS
+      throw new Error('Query parameter "template" has to be a string.');
+    }
+
+    let html: string;
+    switch (req.query.template) {
+      case TemplateKey.Article: {
+        html = getArticleTemplateHtml(req.query);
+        break;
+      }
+
+      default: {
+        // noinspection ExceptionCaughtLocallyJS
+        throw new Error(
+          'Query parameter "template" refers to an unknown template.'
+        );
+      }
+    }
+
+    const screenshot = await getScreenshot(html);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', `image/jpeg`);
+    res.setHeader(
+      'Cache-Control',
+      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+    );
+    res.end(screenshot);
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(500).json({ message: e.message });
+    } else if (e instanceof ZodError) {
+      res.status(500).json({ message: e.message });
+    } else {
+      res.status(500).json({ message: 'Unknown error.' });
+    }
+  }
+};
+
+export default GenerateApiHandler;
---
+++ templates/index.ts
@@ -0,0 +1,19 @@
+import { ArticleTemplateParams, parseArticleTemplateParams } from './article';
+import { NextApiRequest } from 'next';
+
+export enum TemplateKey {
+  Article = 'article',
+}
+
+export type TemplateParams = ArticleTemplateParams;
+
+export type TemplateParamsParseFunction = (
+  query: NextApiRequest['query']
+) => TemplateParams;
+
+export const templateParamsParseFunctionMap: Record<
+  TemplateKey,
+  TemplateParamsParseFunction
+> = {
+  [TemplateKey.Article]: parseArticleTemplateParams,
+};
