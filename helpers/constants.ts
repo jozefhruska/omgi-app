77d870f15dfddb1d673f594c252701bbd43802a9
Jozef Hruška <me@jozefhruska.com>
2022-04-03 11:35:39 +0200

    fix: deployment url (#25)

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,3 +1,5 @@
-export const DEPLOYMENT_URL = process.env.VERCEL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000';
+export const DEPLOYMENT_URL =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'development'
+    ? 'http://localhost:3000'
+    : process.env.NEXT_PUBLIC_DEPLOYMENT_URL ??
+      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
